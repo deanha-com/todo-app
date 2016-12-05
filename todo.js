0b2e8b82426e9c7c25e6cbca26cc20806c2c1aed
Dean Ha <dha@activtrades.com>
2016-12-05 18:26:19 +0000

    added local storage and filter function

--- todo.js
+++ todo.js
@@ -1,18 +1,23 @@
 'use strict';
 
-
 var TodoList = function($container) {
 
-	var todos = [];
+	var todos = [{id: 0, label: 'gotta eat', done: false},
+							{id: 1, label: 'gotta play.. MADNESS', done: false},
+							{id: 2, label: 'True my true', done: true}, {id: 3, label: 'Yoyo', done: true}];
+	// loadData();
 
 	// TODO functions
 
-	function addTodo(todoItem) {
-		todos.push(todoItem);
+	function addTodo(todoItem, done) {
+		var object = {label: todoItem, done : done}; 
+
+		todos.push(object);
 
 		return todos;
 	}
 
+	//This changes the state of the chosen task
 	function todoDone(id, state) {
 		todos[id].done = state;
 		return todos;
@@ -25,9 +30,35 @@ var TodoList = function($container) {
 		return todos;
 	}
 
+	function filterTodos() {
+		var filteredList = [];
+		// for (var i = 0; i < todos.length; i++) {
+
+		// 	if(todos[i].done === false) {
+		// 		filteredList.push(todos[i]);	
+		// 	}
+		// }
+		
+		filteredList = todos.filter(function (todo) {
+			return todo.done;
+		});
+
+		console.log(filteredList);
+	}
+
+	// function loadData() {
+	// 	todos = JSON.parse(localStorage.myTodo);
+	// }
+
 	function saveData() {
 		// save cookie with data
 		var todoJson = JSON.stringify(todos);
+		console.log(todoJson);
+
+		localStorage.setItem("myTodo", todoJson); // localStorage.lastname = "Smith";
+		//localStorage.getItem("mtTodo");  // localStorage.lastname;
+		//localStorage.removeItem("lastname");
+
 	}
 
 	// HTML functions
@@ -35,8 +66,11 @@ var TodoList = function($container) {
 	// 2. Render function
 
 	function render() {
-
+			// todos = localStorage.myTodo;
 	}
+
+
+
 
 	// 3. Listen for changes
 	// 3a. On every change:
@@ -48,7 +82,10 @@ var TodoList = function($container) {
 		addTodo: addTodo,
 		todoDone: todoDone,
 		removeTodo: removeTodo,
-		list: todos
+		saveData: saveData,
+		todolist: todos,
+		render: render,
+		filterTodos: filterTodos
 	}
 
 };
@@ -60,5 +97,6 @@ var todos = new TodoList();
 /* Resources*//*
 
 http://stackoverflow.com/questions/16491758/remove-objects-from-array-by-object-property
+http://www.w3schools.com/html/html5_webstorage.asp
 
 */
